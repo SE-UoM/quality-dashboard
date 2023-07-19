98f739b87156e4f6364fd725f3a199b1a3551955
ArchontisKostis <arxontisk02@gmail.com>
2023-07-19 16:04:36 +0300

    update javadoc

--- backend/src/main/java/gr/uom/strategicplanning/analysis/github/GithubApiClient.java
+++ backend/src/main/java/gr/uom/strategicplanning/analysis/github/GithubApiClient.java
@@ -27,7 +27,7 @@ public class GithubApiClient extends HttpClient {
      * Fetches project data from the GitHub API and populates the provided Project object with the retrieved information.
      *
      * @param project the Project object to populate with project data
-     * @throws IOException if an I/O error occurs during the API request
+     * @throws Exception if an I/O error occurs during the API request
      */
     public void fetchProjectData(Project project) throws Exception {
 
@@ -41,6 +41,14 @@ public class GithubApiClient extends HttpClient {
         project.setStars(getStargazersCount(username, repoName));
     }
 
+    /**
+     * Retrieves the number of stargazers for a GitHub repository.
+     *
+     * @param username the username of the repository owner
+     * @param repoName the name of the repository
+     * @return the number of stargazers for the repository, or 0 if an error occurs
+     * @throws Exception if an error occurs during the API request
+     */
     public int getStargazersCount(String username, String repoName) throws Exception {
         String url = String.format("https://api.github.com/repos/%s/%s/stargazers", username, repoName);
         ResponseEntity response = this.restTemplate.getForEntity(url, Void.class);
@@ -53,6 +61,12 @@ public class GithubApiClient extends HttpClient {
         }
     }
 
+    /**
+     * Entry point of the GithubApiClient class for testing.
+     *
+     * @param args command-line arguments
+     * @throws Exception if an error occurs during the API request
+     */
 
     public static void main(String[] args) throws Exception {
 
@@ -63,16 +77,34 @@ public class GithubApiClient extends HttpClient {
         githubApiClient.fetchProjectData(project1);
     }
 
+    /**
+     * Extracts the username from a GitHub repository URL.
+     *
+     * @param repoUrl the URL of the GitHub repository
+     * @return the username of the repository owner
+     */
     private String extractUsername(String repoUrl) {
         String[] urlParts = repoUrl.split("/");
         return urlParts[urlParts.length - 2];
     }
 
+    /**
+     * Extracts the repository name from a GitHub repository URL.
+     *
+     * @param repoUrl the URL of the GitHub repository
+     * @return the name of the repository
+     */
     private String extractRepoName(String repoUrl) {
         String[] urlParts = repoUrl.split("/");
         return urlParts[urlParts.length - 1];
     }
 
+    /**
+     * Captures the total number of commits for the repository.
+     *
+     * @return the total number of commits for the repository
+     * @throws Exception if an error occurs during the API request
+     */
     private int captureTotalCommits() throws Exception {
         return this.commitService.getCommits(repository).size();
     }
