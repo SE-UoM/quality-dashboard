a96f87a1fd30253240d64cc731b22a72b73bbb44
ArchontisKostis <arxontisk02@gmail.com>
2023-07-23 13:12:38 +0300

    Add JavaDoc comments to SonarScanner

--- backend/src/main/java/gr/uom/strategicplanning/analysis/sonarqube/SonarScanner.java
+++ backend/src/main/java/gr/uom/strategicplanning/analysis/sonarqube/SonarScanner.java
@@ -7,11 +7,20 @@ import org.sonarsource.scanner.api.StdOutLogOutput;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A utility class for scanning projects with SonarQube.
+ */
 public class SonarScanner {
     private EmbeddedScanner scanner;
     private LogOutput logOutput;
     private Map<String, String> sonarProperties;
 
+    /**
+     * Constructs a new SonarScanner instance with the provided name and version.
+     *
+     * @param name    The name of the project.
+     * @param version The version of the project.
+     */
     public SonarScanner(String name, String version) {
         this.logOutput = new StdOutLogOutput();
         this.scanner = EmbeddedScanner.create(name, version, this.logOutput);
@@ -19,6 +28,13 @@ public class SonarScanner {
         initProps();
     }
 
+    /**
+     * Initializes default SonarQube properties.
+     * The default properties are:
+     * - sonar.host.url: http://localhost:9000
+     * - sonar.login: admin
+     * - sonar.password: admin1
+     */
     private void initProps() {
         this.sonarProperties.put("sonar.host.url", "http://localhost:9000");
         this.sonarProperties.put("sonar.login", "admin");
@@ -26,15 +42,28 @@ public class SonarScanner {
         this.sonarProperties.put("sonar.scm.disabled", "true");
     }
 
+    /**
+     * Adds a custom property to be used in the SonarQube analysis.
+     *
+     * @param key   The key of the property.
+     * @param value The value of the property.
+     * Example: addProperty("sonar.projectKey", "test");
+     */
     public void addProperty(String key, String value) {
         this.sonarProperties.put(key, value);
     }
 
+    /**
+     * Starts the SonarQube scanner and performs the analysis using the configured properties.
+     */
     public void start() {
         this.scanner.addGlobalProperties(this.sonarProperties);
         this.scanner.start();
     }
 
+    /**
+     * Executes the SonarQube scanner to analyze the project with the configured properties.
+     */
     public void execute() {
         this.scanner.execute(this.sonarProperties);
     }
