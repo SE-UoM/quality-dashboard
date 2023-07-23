040b6b19e0c717c7b55f379e355227310d31cb63
ArchontisKostis <arxontisk02@gmail.com>
2023-07-23 16:22:37 +0300

    Remove security config

--- backend/src/main/java/gr/uom/strategicplanning/services/UserService.java
+++ backend/src/main/java/gr/uom/strategicplanning/services/UserService.java
@@ -1,37 +1,37 @@
-package gr.uom.strategicplanning.services;
-
-import gr.uom.strategicplanning.models.User;
-import gr.uom.strategicplanning.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class UserService {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    public User createUser(User user) {
-        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
-        if(!userOptional.isPresent()){
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            user.setRoles("SIMPLE");
-            user.setVerified(false);
-            userRepository.save(user);
-        }
-        throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Email is used from another user");
-    }
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-}
+//package gr.uom.strategicplanning.services;
+//
+//import gr.uom.strategicplanning.models.User;
+//import gr.uom.strategicplanning.repositories.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class UserService {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Autowired
+//    PasswordEncoder passwordEncoder;
+//
+//    public User createUser(User user) {
+//        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+//        if(!userOptional.isPresent()){
+//            user.setPassword(passwordEncoder.encode(user.getPassword()));
+//            user.setRoles("SIMPLE");
+//            user.setVerified(false);
+//            userRepository.save(user);
+//        }
+//        throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Email is used from another user");
+//    }
+//
+//    public List<User> getAllUsers() {
+//        return userRepository.findAll();
+//    }
+//}
