3e422ae12b9fa11f774171228e6ef1bf59796945
ArchontisKostis <arxontisk02@gmail.com>
2023-07-29 20:24:14 +0300

    Implement our own tostring and make the double and int to be Number instead

--- backend/src/main/java/gr/uom/strategicplanning/models/Commit.java
+++ backend/src/main/java/gr/uom/strategicplanning/models/Commit.java
@@ -1,9 +1,6 @@
 package gr.uom.strategicplanning.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -25,14 +22,47 @@ public class Commit {
     private Date commitDate;
     @OneToMany
     private Collection<CodeSmell> codeSmells;
-    private float technicalDebt;
-    private int totalFiles;
-    private int totalLoC;
-    private int totalCodeSmells;
-    private float techDebtPerLoC;
+    private Number technicalDebt;
+    private Number totalFiles;
+    private Number totalLoC;
+    private Number totalCodeSmells;
+    private Number techDebtPerLoC;
     @OneToMany
     private Collection<Language> languages;
     private int totalLanguages;
     @ManyToOne
     private Project project;
+
+    public void setCodeSmells(Collection<CodeSmell> codeSmells) {
+        this.codeSmells = codeSmells;
+        this.totalCodeSmells = codeSmells.size();
+    }
+
+    public void setLanguages(Collection<Language> languages) {
+        this.languages = languages;
+        this.totalLanguages = languages.size();
+    }
+
+    public void calculateTechDebtPerLoC() {
+        this.techDebtPerLoC = this.technicalDebt.doubleValue() / this.totalLoC.doubleValue();
+    }
+
+    @Override
+    // Pretty print the Commit object
+    public String toString() {
+        return "Commit{\n" +
+                "id=" + id + "\n" +
+                ", hash='" + hash + '\'' + "\n" +
+                ", developer=" + developer + "\n" +
+                ", commitDate=" + commitDate + "\n" +
+                ", codeSmells=" + codeSmells + "\n" +
+                ", technicalDebt=" + technicalDebt + "\n" +
+                ", totalFiles=" + totalFiles + "\n" +
+                ", totalLoC=" + totalLoC + "\n" +
+                ", totalCodeSmells=" + totalCodeSmells + "\n" +
+                ", techDebtPerLoC=" + techDebtPerLoC + "\n" +
+                ", languages=" + languages + "\n" +
+                ", totalLanguages=" + totalLanguages + "\n" +
+                '}';
+    }
 }
