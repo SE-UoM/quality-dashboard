362db03c227e8a56c1eff7afd9a73729ae6b5716
ArchontisKostis <arxontisk02@gmail.com>
2023-08-08 16:40:53 +0300

    Update LanguageRepository.java
    Use optional

--- backend/src/main/java/gr/uom/strategicplanning/repositories/LanguageRepository.java
+++ backend/src/main/java/gr/uom/strategicplanning/repositories/LanguageRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Long> {
     List<Language> findByProject(Project project);
 
-    Language findByName(String languageName);
+    Optional<Language> findByName(String languageName);
 }
