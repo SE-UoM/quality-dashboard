d7a014888415455c1154971d870abbf9bdd22db8
ArchontisKostis <arxontisk02@gmail.com>
2023-09-01 17:21:50 +0300

    Implement specific endpoint to get the names of the organization projects
    This endpoint can be used on the footer.

--- backend/src/main/java/gr/uom/strategicplanning/controllers/OrganizationController.java
+++ backend/src/main/java/gr/uom/strategicplanning/controllers/OrganizationController.java
@@ -7,11 +7,13 @@ import gr.uom.strategicplanning.models.domain.Organization;
 import gr.uom.strategicplanning.models.domain.Project;
 import gr.uom.strategicplanning.models.stats.ProjectStats;
 import gr.uom.strategicplanning.repositories.OrganizationRepository;
+import gr.uom.strategicplanning.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.*;
 import java.util.stream.Collectors;
 @RestController
@@ -20,6 +22,8 @@ public class OrganizationController {
 
     @Autowired
     private OrganizationRepository organizationRepository;
+    @Autowired
+    private OrganizationService organizationService;
 
     @GetMapping
     public ResponseEntity<List<OrganizationResponse>> getAllOrganizations() {
@@ -46,32 +50,58 @@ public class OrganizationController {
         }
     @GetMapping("/{id}/projects-info")
     public ResponseEntity<Collection<Map>> getOrganizationProjectsInfo(@PathVariable Long id) {
-        Optional<Organization> organizationOptional = organizationRepository.findById(id);
+        try {
+            Organization organization = organizationService.getOrganizationById(id);
 
-        if (organizationOptional.isEmpty())
-            return ResponseEntity.notFound().build();
+            Collection<Project> organizationProjects = organization.getProjects();
+            Collection<Map> projectsInfoResponse = new ArrayList<>();
 
-        Organization organization = organizationOptional.get();
+            for (Project project : organizationProjects) {
+                String projectName = project.getName();
+                int projectCommits = project.getTotalCommits();
+                int getProjectStars = project.getStars();
+                int projectForks = project.getForks();
 
-        Collection<Project> organizationProjects = organization.getProjects();
-        Collection<Map> projectsInfoResponse = new ArrayList<>();
+                Map<String, Object> projectInfoMap = new HashMap<>();
+                projectInfoMap.put("name", projectName);
+                projectInfoMap.put("totalContributions", projectCommits);
+                projectInfoMap.put("stars", getProjectStars);
+                projectInfoMap.put("forks", projectForks);
 
-        for (Project project : organizationProjects) {
-            String projectName = project.getName();
-            int projectCommits = project.getTotalCommits();
-            int getProjectStars = project.getStars();
-            int projectForks = project.getForks();
+                projectsInfoResponse.add(projectInfoMap);
+            }
 
-            Map<String, Object> projectInfoMap = new HashMap<>();
-            projectInfoMap.put("name", projectName);
-            projectInfoMap.put("totalContributions", projectCommits);
-            projectInfoMap.put("stars", getProjectStars);
-            projectInfoMap.put("forks", projectForks);
-
-            projectsInfoResponse.add(projectInfoMap);
+            return ResponseEntity.ok(projectsInfoResponse);
         }
+        catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-        return ResponseEntity.ok(projectsInfoResponse);
+    @GetMapping("/{id}/project-names")
+    public ResponseEntity<Collection<Map>> getOrganizationProjectNames(@PathVariable Long id) {
+        try {
+            Organization organization = organizationService.getOrganizationById(id);
+
+            Collection<Project> organizationProjects = organization.getProjects();
+            Collection<Map> projectsInfoResponse = new ArrayList<>();
+
+            for (Project project : organizationProjects) {
+                String projectName = project.getName();
+
+                Map<String, Object> projectInfoMap = new HashMap<>();
+                projectInfoMap.put("name", projectName);
+
+                projectsInfoResponse.add(projectInfoMap);
+            }
+
+            return ResponseEntity.ok(projectsInfoResponse);
+        }
+        catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/organization-analysis")
