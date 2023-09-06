bd8b67dd1f247437a5a588ded2df7302810b42f6
ArchontisKostis <arxontisk02@gmail.com>
2023-09-06 15:39:34 +0300

    Update OrganizationController.java
    Co-authored-by: @GeorgeApos
    Co-authored-by: @GeorgeFkd
    Co-authored-by: @nikosnikolaidis
    Co-authored-by: @teomaik

--- backend/src/main/java/gr/uom/strategicplanning/controllers/OrganizationController.java
+++ backend/src/main/java/gr/uom/strategicplanning/controllers/OrganizationController.java
@@ -28,7 +28,6 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/organizations")
 public class OrganizationController {
-
     @Autowired
     private OrganizationRepository organizationRepository;
     @Autowired
