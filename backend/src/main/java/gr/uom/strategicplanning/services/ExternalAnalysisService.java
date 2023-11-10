e9a91727676093762e092a4cee1c2568e90adfcb
ArchontisKostis <arxontisk02@gmail.com>
2023-11-10 14:26:45 +0200

    Update ExternalAnalysisService.java
    Added a logger and did some refactoring.
    PROBLEMS:
    - I cant analyze with pyasses
    - generally i can call the external services if i run locally but not if i run with docker-compose

--- backend/src/main/java/gr/uom/strategicplanning/services/ExternalAnalysisService.java
+++ backend/src/main/java/gr/uom/strategicplanning/services/ExternalAnalysisService.java
@@ -5,7 +5,12 @@ import gr.uom.strategicplanning.analysis.external.strategies.PyAssessServiceStra
 import gr.uom.strategicplanning.enums.ProjectStatus;
 import gr.uom.strategicplanning.models.domain.Project;
 import gr.uom.strategicplanning.models.exceptions.ExternalAnalysisException;
+import io.swagger.models.auth.In;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +19,7 @@ import java.util.Map;
 
 @Service
 public class ExternalAnalysisService {
+    Logger logger = LoggerFactory.getLogger(ExternalAnalysisService.class);
     private final PyAssessServiceStrategy pyAssessServiceStrategy;
     private final CodeInspectorServiceStrategy codeInspectorServiceStrategy;
 
@@ -23,7 +29,7 @@ public class ExternalAnalysisService {
     private String CODE_INSPECTOR_URL;
 
     private String CODE_INSPECTOR_ANALYZE_HOTSPOTS = "/api/analysis/prioritize_hotspots";
-    private String PYASSESS_ANALYZE_REPO = "/api/analysis";
+    private String PYASSESS_ANALYZE_REPO = "/project_analysis";
 
     public ExternalAnalysisService(RestTemplate restTemplate) {
         this.pyAssessServiceStrategy = new PyAssessServiceStrategy(restTemplate);
@@ -42,45 +48,81 @@ public class ExternalAnalysisService {
         }
     }
 
+    private boolean analyzeHotspotsWithCodeInspector(Project project) {
+        logger.info("Beggining analysis with CodeInspector");
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("endpointUrl", CODE_INSPECTOR_URL + CODE_INSPECTOR_ANALYZE_HOTSPOTS);
+        params.put("gitUrl", project.getRepoUrl());
+        params.put("method", HttpMethod.GET);
+
+        logger.info("Sending Analysis Request to CodeInspector");
+        codeInspectorServiceStrategy.sendRequest(params);
+        project.setCodeInspectorStatus(ProjectStatus.ANALYSIS_COMPLETED);
+        logger.info("CodeInspector analysis completed");
+
+        return true;
+    }
+
+    private boolean analyzeWithPyAssess(Project project, String branch) {
+        // I get this from PyAssess
+        // 2023-11-10 14:09:06 2023-11-10 12:09:06.144
+        // WARN 1 --- [nio-8080-exec-4] .w.s.m.s.DefaultHandlerExceptionResolver :
+        // Resolved [org.springframework.web.bind.MissingServletRequestParameterException:
+        // Required request parameter 'branch' for method parameter type String is not present]
+        // I added the branch parameter to the method but how do we actually get the branch name?
+
+        if (!project.hasLanguage("Python")) return false;
+
+        // We won't get to this point if the project doesn't have Python as a language
+        Map<String, Object> params = new HashMap<>();
+        logger.info("Beggining analysis with PyAssess");
+        params.put("endpointUrl", PYASSESS_URL + PYASSESS_ANALYZE_REPO);
+        params.put("gitUrl", project.getRepoUrl());
+        params.put("method", HttpMethod.POST);
+        params.put("token", null);
+        params.put("ciToken", null);
+
+        logger.info("Sending request to PyAssess");
+        pyAssessServiceStrategy.sendRequest(params);
+        project.setPyAssessStatus(ProjectStatus.ANALYSIS_COMPLETED);
+        logger.info("PyAssess analysis completed");
+
+        return true;
+    }
+
     public boolean analyzeWithExternalServices(Project project) {
         try {
-            // Analysis with CodeInspector
-            Map<String, String> params = new HashMap<>();
-            params.put("endpointUrl", CODE_INSPECTOR_URL + CODE_INSPECTOR_ANALYZE_HOTSPOTS);
-            params.put("gitUrl", project.getRepoUrl());
-
-            codeInspectorServiceStrategy.sendRequest(params);
-            project.setCodeInspectorStatus(ProjectStatus.ANALYSIS_COMPLETED);
-
-            if (!project.hasLanguage("Python")) return false;
-
-            // We won't get to this point if the project doesn't have Python as a language
-
-            // Clear params for next request
-            params.clear();
-
-            // Analysis with PyAssess
-            params.put("endpointUrl", PYASSESS_URL + PYASSESS_ANALYZE_REPO);
-            params.put("gitUrl", project.getRepoUrl());
-            params.put("token", null);
-            params.put("ciToken", null);
-            pyAssessServiceStrategy.sendRequest(params);
-            // fakeAnalysis("PyAssess", 1000);
+            analyzeHotspotsWithCodeInspector(project);
+            analyzeWithPyAssess(project, "master");
         }
         catch (Exception e) {
-            if (e instanceof ExternalAnalysisException) {
-                ExternalAnalysisException casted = (ExternalAnalysisException) e;
-
-                String serviceName = casted.getExternalServiceName();
-
-                if (serviceName.equalsIgnoreCase("CodeInspector"))
-                    project.setCodeInspectorStatus(ProjectStatus.ANALYSIS_FAILED);
-
-                if (serviceName.equalsIgnoreCase("PyAssess"))
-                    project.setPyAssessStatus(ProjectStatus.ANALYSIS_FAILED);
-            }
+            handleAnalysisException(e, project);
+            e.printStackTrace();
+            return false;
         }
         return true;
+    }
+
+
+    private void handleAnalysisException(Exception e, Project project) {
+        if (e instanceof ExternalAnalysisException) {
+            ExternalAnalysisException casted = (ExternalAnalysisException) e;
+
+            String serviceName = casted.getExternalServiceName();
+
+            if (serviceName.equalsIgnoreCase("CodeInspector")) {
+                project.setCodeInspectorStatus(ProjectStatus.ANALYSIS_FAILED);
+                logger.error("CodeInspector analysis failed");
+            }
+
+            if (serviceName.equalsIgnoreCase("PyAssess")) {
+                project.setPyAssessStatus(ProjectStatus.ANALYSIS_FAILED);
+                logger.error("PyAssess analysis failed");
+            }
+        }
+
+        logger.error("External analysis failed");
     }
 
 }
