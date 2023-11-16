cf226da2f18155567b8eac186f82233ebeb06caf
ArchontisKostis <arxontisk02@gmail.com>
2023-11-16 10:13:13 +0200

    Update BestPracticesRepository.java

--- backend/src/main/java/gr/uom/strategicplanning/repositories/BestPracticesRepository.java
+++ backend/src/main/java/gr/uom/strategicplanning/repositories/BestPracticesRepository.java
@@ -4,6 +4,9 @@ import gr.uom.strategicplanning.models.domain.BestPractice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BestPracticesRepository extends JpaRepository<BestPractice, Long> {
+    public Optional<BestPractice> findByTitle(String title);
 }
