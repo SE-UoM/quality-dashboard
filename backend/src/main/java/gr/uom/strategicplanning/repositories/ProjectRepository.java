3b7ff18f9e1fcad1e4dfea624c9ce8b90c3425de
ArchontisKostis <arxontisk02@gmail.com>
2024-02-21 16:26:13 +0200

    Add findAllByStatus to ProjectRepository

--- backend/src/main/java/gr/uom/strategicplanning/repositories/ProjectRepository.java
+++ backend/src/main/java/gr/uom/strategicplanning/repositories/ProjectRepository.java
@@ -1,9 +1,11 @@
 package gr.uom.strategicplanning.repositories;
 
+import gr.uom.strategicplanning.enums.ProjectStatus;
 import gr.uom.strategicplanning.models.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,6 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     Optional<Project> findFirstByRepoUrl(String githubUrl);
     Optional<Project> findByName(String name);
+
+    List<Project> findAllByStatus(ProjectStatus status);
 }
