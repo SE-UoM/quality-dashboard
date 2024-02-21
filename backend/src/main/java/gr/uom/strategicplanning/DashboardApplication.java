2249080796873ad185760445b5addae0af69c3d4
ArchontisKostis <arxontisk02@gmail.com>
2024-02-21 16:27:32 +0200

    Update DashboardApplication.java
    Enable Scheduling

--- backend/src/main/java/gr/uom/strategicplanning/DashboardApplication.java
+++ backend/src/main/java/gr/uom/strategicplanning/DashboardApplication.java
@@ -2,8 +2,10 @@ package gr.uom.strategicplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DashboardApplication {
 
 	public static void main(String[] args) {
