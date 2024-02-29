67a2850911470e0a840c2b5f4199326797b3c916
ArchontisKostis <arxontisk02@gmail.com>
2024-02-29 12:34:58 +0200

    init discovery service

---
+++ backend/discovery-service/src/test/java/uom/qualitydashboard/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uom.qualitydashboard.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
