807546997d922d7e1c1f2b684aa8aa51a09e22eb
ArchontisKostis <arxontisk02@gmail.com>
2024-03-05 10:40:21 +0200

    init developer service

---
+++ backend/developer-service/src/main/java/uom/qualitydashboard/developerservice/DeveloperServiceApplication.java
@@ -0,0 +1,13 @@
+package uom.qualitydashboard.developerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperServiceApplication.class, args);
+	}
+
+}
---
+++ backend/developer-service/src/test/java/uom/qualitydashboard/developerservice/DeveloperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uom.qualitydashboard.developerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
