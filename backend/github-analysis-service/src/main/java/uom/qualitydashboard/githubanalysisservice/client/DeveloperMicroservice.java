8fffbc12f6497d6f82384800b3bc98de3d649e56
ArchontisKostis <arxontisk02@gmail.com>
2024-03-05 16:30:13 +0200

    connect github analysis with developer microservice

--- backend/github-analysis-service/src/main/java/uom/qualitydashboard/githubanalysisservice/client/DeveloperMicroservice.java
+++ backend/github-analysis-service/src/main/java/uom/qualitydashboard/githubanalysisservice/client/DeveloperMicroservice.java
@@ -1,6 +1,7 @@
 package uom.qualitydashboard.githubanalysisservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uom.qualitydashboard.githubanalysisservice.models.CreateDeveloperRequest;
 import uom.qualitydashboard.githubanalysisservice.models.DeveloperDTO;
@@ -13,8 +14,8 @@ public interface DeveloperMicroservice {
     Optional<DeveloperDTO> getDeveloperById(@PathVariable Long developerId);
 
     @PostMapping("/create")
-    Optional<DeveloperDTO> createDeveloper(@RequestBody CreateDeveloperRequest request);
+    DeveloperDTO createDeveloper(@RequestBody CreateDeveloperRequest request);
 
     @GetMapping("/github-profile-url")
-    Optional<DeveloperDTO> getDeveloperByGithubProfileURL(@RequestParam(name = "key") String githubProfileURL);
+    ResponseEntity<?> getDeveloperByGithubProfileURL(@RequestParam(name = "key") String githubProfileURL);
 }
