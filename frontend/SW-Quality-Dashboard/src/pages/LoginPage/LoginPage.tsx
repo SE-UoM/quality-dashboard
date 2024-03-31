1fa344ca3ebb7553090ac29e46e8bf73ab7bdd68
Archontis Kostis <arxontisk02@gmail.com>
2024-03-31 22:48:51 +0300

    Implement Basic Login Page

---
+++ frontend/SW-Quality-Dashboard/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Box, Flex, Button, FormControl, FormLabel, Input, Grid } from '@chakra-ui/react';
+import LoginForm from "../components/LoginForm/LoginForm.tsx";
+
+function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+
+    const handleLogin = () => {
+        // Placeholder function for login logic
+        console.log('Login');
+        console.log('Username:', username);
+    }
+
+    return (
+        <div className={'login-page'}>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default LoginPage
