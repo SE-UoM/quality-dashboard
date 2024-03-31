50ee7a753d097eb62f73a2c78c63975b9c9c84de
Archontis Kostis <arxontisk02@gmail.com>
2024-03-31 15:23:11 +0300

    Implement Mail Verification Page

--- frontend/SW-Quality-Dashboard/src/pages/VerifyUserPage.tsx
+++ frontend/SW-Quality-Dashboard/src/pages/VerifyUserPage.tsx
@@ -1,39 +1,46 @@
 import React, {useEffect} from 'react'
-import {useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import logo from '../assets/dashboard_logo_transparent.png'
 import axios from "axios";
+import {Button} from "@chakra-ui/react";
 
 function VerifyUserPage() {
     const location = useLocation()
     const [statusTextTitle, setStatusTextTitle] = React.useState<string>('User Verification in Progress')
-    const [statusText, setStatusText] = React.useState<string>('Please wait while we verify your account')
+    const [statusText, setStatusText] = React.useState<string>('Verifying your email address. Please wait...')
+    const [verificationSuccess, setVerificationSuccess] = React.useState<boolean>(false)
 
     useEffect(() => {
         let params = new URLSearchParams(location.search)
-        console.log(params)
         let token = params.get('token')
-        console.log(token)
-
         let uid = params.get('uid')
-        console.log(uid)
 
+        // URI Encode the token
+        token = encodeURIComponent(token)
         let url = `http://localhost:8080/api/user/verify?token=${token}&uid=${uid}`
-        console.log(url)
 
-        axios.post(url)
+        // Wait a few seconds to make the user know something is happening
+        setTimeout(() => {
+            callAPI(token, uid)
+        }  , 1500)
+    }, []);
+
+    async function callAPI(token, uid) {
+        let url = `http://localhost:8080/api/user/verify?token=${token}&uid=${uid}`
+        await axios.post(url)
             .then(response => {
                 if (response.status === 200) {
                     setStatusTextTitle('User Verification Successful')
                     setStatusText('Your account has been verified successfully. You can now login to your account and start analyzing projects.')
+                    setVerificationSuccess(true)
                 }
             })
             .catch(error => {
                 console.error('Error verifying email:', error);
                 setStatusText('Error verifying email. Please contact your admin.'); // Set verification status to error message
+                setVerificationSuccess(false)
             });
-
-
-    }, []);
+    }
 
     let pageStyle = {
         display: 'flex',
@@ -53,8 +60,24 @@ function VerifyUserPage() {
                     <i><b>{statusTextTitle}</b></i>
                 </h1>
 
-                <p>
+                <p
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                    }}
+                >
                     {statusText}
+                    <Link to="/"
+                        style={{
+                            marginTop: '1em',
+                            fontStyle: 'italic',
+                            color: '#015092',
+                            textDecoration: 'underline',
+                        }}
+                    >
+                        Go to Home page
+                    </Link>
                 </p>
 
             </div>
