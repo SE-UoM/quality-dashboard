6ba453c86d2eba035c750fe861c91709f94aa82f
Archontis Kostis <arxontisk02@gmail.com>
2024-04-01 16:51:15 +0300

    Remove useless code from Login page

--- frontend/SW-Quality-Dashboard/src/pages/LoginPage/LoginPage.tsx
+++ frontend/SW-Quality-Dashboard/src/pages/LoginPage/LoginPage.tsx
@@ -4,16 +4,6 @@ import LoginForm from "../../components/LoginForm/LoginForm.tsx";
 import './LoginPage.css';
 
 function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordError, setPasswordError] = useState(false);
-
-    const handleLogin = () => {
-        // Placeholder function for login logic
-        console.log('Login');
-        console.log('Username:', username);
-    }
-
     return (
         <div className={'login-page'}>
             <LoginForm />
