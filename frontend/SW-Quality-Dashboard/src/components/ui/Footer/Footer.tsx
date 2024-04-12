ede1c4f0e88b39ab2732e0806f9f9a0e173708a9
Archontis Kostis <arxontisk02@gmail.com>
2024-04-12 14:00:49 +0300

    UI Enhancements

--- frontend/SW-Quality-Dashboard/src/components/ui/Footer/Footer.tsx
+++ frontend/SW-Quality-Dashboard/src/components/ui/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./Footer.css"
 import {Button} from "react-bootstrap";
+import logo from '../../../assets/svg/dashboard_logo_black.svg'
 
 function Footer() {
 
@@ -9,14 +10,48 @@ function Footer() {
         <>
             <footer className="dashboard-footer">
                 <section className={"footer-top"}>
+                    <div className="footer-top-brand">
+                        <img className="footer-logo" src={logo} alt="UoM Quality Dashboard Logo"/>
+                        <p>
+                            UoM Quality Dashboard <br/>
+                            By University of Macedonia, OpenSource Uom and SE Lab Uom
+                        </p>
+                    </div>
+
+                    <div className="footer-top-content">
+                        <div className="footer-top-content-item">
+                            <h3>Quick Links</h3>
+                            <ul>
+                                <li><a href="https://www.uom.gr/dai">Department of Applied Informatics</a></li>
+                                <li><a href="https://sde.uom.gr/">Software and Data Engineering Lab</a></li>
+                                <li><a href="https://opensource.uom.gr/">OpenSource UoM Team</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-top-content-item">
+                            <h3>Resources</h3>
+                            <ul>
+                                <li><a href="https://github.com/SE-UoM/quality-dashboard">Github Repository</a></li>
+                                <li><a href="https://github.com/SE-UoM/quality-dashboard/issues">Report a Bug</a></li>
+                                <li><a href="">Contact</a></li>
+                            </ul>
+                        </div>
+
+                        <div className="footer-top-content-item">
+                            <h3>More Links</h3>
+                            <ul>
+                                <li><a href="https://my.uom.gr/">myUoM</a></li>
+                                <li><a href="https://opensource.uom.gr/index.php/blog/">OpenSource UoM Blog</a></li>
+                                <li><a href="https://sde.uom.gr/index.php/research-groups/software-engineering-group/">SDE UoM Software Engineering Group</a></li>
+                            </ul>
+                        </div>
+
+
+                    </div>
 
                 </section>
 
                 <section className={"footer-bottom"}>
-                    <p>© {new Date().getFullYear()} UoM Quality Dashboard</p>
-                    <p>
-                        Made by <a href="https://opensource.uom.gr/uom" className="attribution-url"> OpenSourceUom</a>, <a href={"https://www.uom.gr/"} className="attribution-url">SE Lab Uom</a> and <a href={"https://www.uom.gr/"} className="attribution-url"> University of Macedonia</a>
-                    </p>
+                    <p>{new Date().getFullYear()} UoM Quality Dashboard</p>
                 </section>
             </footer>
         </>
--- frontend/SW-Quality-Dashboard/src/pages/general/HomePage/HomePage.tsx
+++ frontend/SW-Quality-Dashboard/src/pages/general/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import {Button, Image} from 'react-bootstrap';
 import useLocalStorage from '../../../hooks/useLocalStorage.ts';
-import axios from 'axios';
+import openSourceLogo from '../../../assets/img/OS_UoM_Logo.png';
+import sdeLabLogo from '../../../assets/img/sde-banner.png';
 import './HomePage.css';
 import useAuthenticationCheck from "../../../hooks/useAuthenticationCheck.ts";
+import {Link} from "react-router-dom";
 
 function HomePage() {
     const [accessToken] = useLocalStorage<string>('accessToken', '');
@@ -23,6 +25,20 @@ function HomePage() {
                 {isAuthenticated !== null && !isAuthenticated && <Button href="/login" variant="outline-light">Login</Button>}
                 {isAuthenticated !== null && !isAuthenticated && <Button href="/register" variant="outline-light">Register</Button>}
             </section>
+
+            <div className="homepage-images">
+                <Link className="home-page-img-link" to="https://opensource.uom.gr">
+                    <Image className="img" height="60vh" src={openSourceLogo} alt="OpenSource UoM Logo" rounded/>
+                </Link>
+
+                <Link className="home-page-img-link" to={"https://sde.uom.gr"}>
+                    <Image className="img" height="60vh" src={"https://sde.uom.gr/wp-content/uploads/2016/10/sde-banner.png"} alt="Software and Data Engineering Lab Logo" rounded/>
+                </Link>
+
+                <Link className="home-page-img-link" to={"https://www.uom.gr/site/en"}>
+                    <Image className="img" id="uom" height="60vh" src="https://www.uom.gr/site/images/logos/UOMLOGOEN.png" alt="University of Macedonia Logo" rounded/>
+                </Link>
+            </div>
         </div>
     );
 }
