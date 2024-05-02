2ab092dc436b5df4ea85eb55b36e8b12795b800c
Archontis Kostis <arxontisk02@gmail.com>
2024-05-02 23:20:12 +0300

    Make Home Page Use Daisy UI

--- frontend/SW-Quality-Dashboard/src/components/ui/DashboardNavbar/DashboardNavbar.tsx
+++ frontend/SW-Quality-Dashboard/src/components/ui/DashboardNavbar/DashboardNavbar.tsx
@@ -11,95 +11,78 @@ function DashboardNavbar({isAuthenticated, isAdmin}) {
 
     return (
         <>
+            <div className="navbar bg-base-100">
+                {/* NAVBAR LOGO */}
+                <div className="flex-1">
+                    <a href="/" className="navbar-brand">
+                        <img src={logo}/>
+                        <section className="navbar-brand-text">
+                            <span>University of Macedonia</span>
+                            <h1>Quality Dashboard</h1>
+                        </section>
+                    </a>
+                </div>
 
-                <Navbar expand="lg" className="bg-body-tertiary" style={{
-                borderBottom: "2px solid var(--org-color-primary-dark-version)",
-                minHeight: "10vh",
-                padding: "0 2rem"
-                }}>
-                    <Container>
-                        <Navbar.Brand href="/" className="navbar-brand">
-                            <img src={logo}/>
-                            <section className="navbar-brand-text">
-                                <span>University of Macedonia</span>
-                                <h1>Quality Dashboard</h1>
-                            </section>
-                        </Navbar.Brand>
+                <div className="flex-none">
+                        <div className="dropdown dropdown-end">
+                            <label className="swap swap-rotate">
 
-                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                        {isAuthenticated !== null &&
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                {!isAuthenticated && (
-                                    <>
-                                        <Nav.Link href="/register">
-                                            <i className="bi bi-person-fill-add"> </i>
-                                            Sign Up
-                                        </Nav.Link>
+                                {/* this hidden checkbox controls the state */}
+                                <input type="checkbox" className="theme-controller" value="light" />
 
-                                        <Nav.Link href="/login">
-                                            <i className="bi bi-person-fill"> </i>
-                                            Login
-                                        </Nav.Link>
-                                    </>
-                                )}
+                                {/* sun icon */}
+                                <svg className="swap-off fill-current w-7 h-7 m-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
 
-                                {isAuthenticated && (
-                                    <>
-                                        <Nav.Link href="/dashboard">
-                                            <i className="bi bi-speedometer2"> </i>
-                                            View Dashboard
-                                        </Nav.Link>
+                                {/* moon icon */}
+                                <svg className="swap-on fill-current w-7 h-7 m-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
 
-                                        <NavDropdown title={<i className="bi bi-rocket-takeoff-fill"
-                                                               style={{fontStyle: "initial"}}> My Actions</i>}
-                                                     id="actions-dropdown">
-                                            {isAdmin &&
-                                                <>
-                                                    <NavDropdown.Item href="/admin" color={"var(--org-color-primary)"}>
-                                                        <i className="bi bi-gear-fill"> </i>
-                                                        Admin Panel
-                                                    </NavDropdown.Item>
-                                                </>
-                                            }
+                            </label>
+                        </div>
 
-                                            <NavDropdown.Item href="/submit-project" color={"var(--org-color-primary)"}>
-                                                <i className="bi bi-plus-circle-fill"> </i>
-                                                Submit a Project
-                                            </NavDropdown.Item>
+                    {isAuthenticated &&
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img alt="Tailwind CSS Navbar component"
+                                         src="https://ui-avatars.com/api/?name=John+Doe"/>
+                                </div>
+                            </div>
+                            <ul tabindex="0"
+                                class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                            >
+                                <li>
+                                    <a href="/dashboard">
+                                        <i className="bi bi-speedometer2"> </i>
+                                        Go to Dashboard
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/submit-project">
+                                        <i className="bi bi-plus-circle-fill"> </i>
+                                        Submit a Project
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/admin">
+                                        <i className="bi bi-gear-fill"> </i>
+                                        Admin Panel
+                                    </a>
+                                </li>
 
-                                            <NavDropdown.Item href="/submit-project" color={"var(--org-color-primary)"}>
-                                                <i className="bi bi-folder-fill"> </i>
-                                                View Submitted Projects
-                                            </NavDropdown.Item>
-
-                                            <NavDropdown.Divider id={"nav-dropdown-logout-divider"}/>
-
-                                            <Nav.Item
-                                                href="/logout"
-                                                id={"nav-logout-link"}
-                                                style={{}}
-
-                                                onClick={handleLogout}
-                                            >
-                                                <i className="bi bi-power"> </i>
-                                                Logout
-                                            </Nav.Item>
-                                        </NavDropdown>
-                                    </>
-                                )}
-                            </Nav>
-                        </Navbar.Collapse>
-                        }
-
-                        <Navbar.Collapse className="justify-content-end">
-                            <a href="https://github.com/SE-UoM/quality-dashboard" id="github-repo-icon">
-                                <i className="bi bi-github"></i>
-                            </a>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-
+                                <li>
+                                    <button
+                                        onClick={handleLogout}
+                                        className="text-error"
+                                    >
+                                        <i className="bi bi-power"> </i>
+                                        Logout
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    }
+                </div>
+            </div>
         </>
     )
 }
--- frontend/SW-Quality-Dashboard/src/pages/general/HomePage/HomePage.tsx
+++ frontend/SW-Quality-Dashboard/src/pages/general/HomePage/HomePage.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import {Button, Image} from 'react-bootstrap';
 import useLocalStorage from '../../../hooks/useLocalStorage.ts';
 import openSourceLogo from '../../../assets/img/OS_UoM_Logo.png';
-import sdeLabLogo from '../../../assets/img/sde-banner.png';
+import homeText from '../../../../content/pages/home.json'
 import './HomePage.css';
 import useAuthenticationCheck from "../../../hooks/useAuthenticationCheck.ts";
 import {Link} from "react-router-dom";
+import bgImg from '../../../assets/img/screen-mockup.png';
 
 function HomePage() {
     const [accessToken] = useLocalStorage<string>('accessToken', '');
@@ -14,33 +15,31 @@ function HomePage() {
     return (
 
         <div className="home-page">
-            <button className="btn">Button</button>
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage: `url(${bgImg})`
 
-            <h1>Welcome to UoM Dashboard</h1>
-            <p>Here you can find all the information you need about the software quality of University of Macedonia</p>
-
-            <section className="homepage-action-buttons">
-                {/* These Will show only if the token we have on local storage works */}
-                {isAuthenticated && <Button href="/submit-project" variant="outline-light">Add Project</Button>}
-                {isAuthenticated && <Button href="/dashboard" variant="outline-light">Dashboard</Button>}
-                {isAuthenticated && <Button href="/" variant="outline-light">View your Projects</Button>}
-
-                {isAuthenticated !== null && !isAuthenticated && <Button href="/login" variant="outline-light">Login</Button>}
-                {isAuthenticated !== null && !isAuthenticated && <Button href="/register" variant="outline-light">Register</Button>}
-            </section>
-
-            <div className="homepage-images">
-                <Link className="home-page-img-link" to="https://opensource.uom.gr">
-                    <Image className="img" height="60vh" src={openSourceLogo} alt="OpenSource UoM Logo" rounded/>
-                </Link>
-
-                <Link className="home-page-img-link" to={"https://sde.uom.gr"}>
-                    <Image className="img" height="60vh" src={"https://sde.uom.gr/wp-content/uploads/2016/10/sde-banner.png"} alt="Software and Data Engineering Lab Logo" rounded/>
-                </Link>
-
-                <Link className="home-page-img-link" to={"https://www.uom.gr/site/en"}>
-                    <Image className="img" id="uom" height="60vh" src="https://www.uom.gr/site/images/logos/UOMLOGOEN.png" alt="University of Macedonia Logo" rounded/>
-                </Link>
+            }}>
+                <div className="hero-overlay bg-opacity-85"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold"
+                            style={{color: 'white'}}
+                        >
+                            {homeText.heroHeader}
+                        </h1>
+                        <p style={{color: 'white'}}>
+                            {homeText.heroText}
+                        </p>
+                        <a
+                            href="/login"
+                            className="btn"
+                        >
+                            {homeText.heroButton}
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     );
