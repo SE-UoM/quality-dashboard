9f37ec77a9b858ce95286d0a9e436a7195a5ebd2
Archontis Kostis <arxontisk02@gmail.com>
2024-05-04 19:08:07 +0300

    Add new more beautiful navbar with more functionality
    - Theme toggle (not completelly working)
    - Auto cycle slides

--- frontend/SW-Quality-Dashboard/src/pages/general/DashboardPage/DashboardPage.tsx
+++ frontend/SW-Quality-Dashboard/src/pages/general/DashboardPage/DashboardPage.tsx
@@ -6,12 +6,16 @@ import {useEffect, useState} from "react";
 import DashboardSlideTwo from "../../../components/dashboard/slides/DashboardSlideTwo/DashboardSlideTwo.tsx";
 import DashboardSlideThree from "../../../components/dashboard/slides/DashboardSlideThree/DashboardSlideThree.tsx";
 import DashboardSlideFour from "../../../components/dashboard/slides/DashboardSlideFour/DashboardSlideFour.tsx";
+import ProtectedRoute from "../../../routes/ProtectedRoute.tsx";
+import SubmitProjectForm from "../../../components/forms/SubmitProjectForm/SubmitProjectForm.tsx";
 
 function DashboardPage({isAuthenticated, isAdmin}) {
     const location = useLocation()
     const navigate = useNavigate()
     const urlParams = new URLSearchParams(location.search)
     const slideNumber = urlParams.get('p')
+
+    const [loadingAuth, setLoadingAuth] = useState<boolean>(true)
 
     const [currentSlide, setCurrentSlide] = useState<number>(
         slideNumber ? parseInt(slideNumber) : 1
@@ -24,30 +28,48 @@ function DashboardPage({isAuthenticated, isAdmin}) {
 
     return (
         <>
-            <CollabsibleNavbar
-                isAuthenticated={isAuthenticated}
-                isAdmin={isAdmin}
-                currentSlide={currentSlide}
-                setCurrentSlide={setCurrentSlide}
-                totalSlides={4}
-            />
-            <div className="dashboard-page">
-                {(!currentSlide || currentSlide === 1) &&
-                    <DashboardSlideOne />
-                }
+            <ProtectedRoute loadingAuth={loadingAuth} setLoadingAuth={setLoadingAuth} />
 
-                {currentSlide === 2 &&
-                    <DashboardSlideTwo />
-                }
+            {loadingAuth ? (
+                <div
+                    className="loading-item"
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100vh"
+                    }}
+                >
+                    <span className="loading loading-infinity loading-lg"></span>
+                </div>
+            ) : (
+                <>
+                    <CollabsibleNavbar
+                        isAuthenticated={isAuthenticated}
+                        isAdmin={isAdmin}
+                        currentSlide={currentSlide}
+                        setCurrentSlide={setCurrentSlide}
+                        totalSlides={4}
+                    />
+                    <div className="dashboard-page bg-primary">
+                        {(!currentSlide || currentSlide === 1) &&
+                            <DashboardSlideOne />
+                        }
 
-                {currentSlide === 3 &&
-                    <DashboardSlideThree />
-                }
+                        {currentSlide === 2 &&
+                            <DashboardSlideTwo />
+                        }
 
-                {currentSlide === 4 &&
-                    <DashboardSlideFour />
-                }
-            </div>
+                        {currentSlide === 3 &&
+                            <DashboardSlideThree />
+                        }
+
+                        {currentSlide === 4 &&
+                            <DashboardSlideFour />
+                        }
+                    </div>
+                </>
+            )}
         </>
     )
 }
