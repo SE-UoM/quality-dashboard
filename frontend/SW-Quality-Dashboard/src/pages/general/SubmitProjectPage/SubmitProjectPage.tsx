1e66a16c0ecb252ce264eb0e57336d9cc638d044
Archontis Kostis <arxontisk02@gmail.com>
2024-05-04 19:07:03 +0300

    Make submitted Project a Protected Route

--- frontend/SW-Quality-Dashboard/src/pages/general/SubmitProjectPage/SubmitProjectPage.tsx
+++ frontend/SW-Quality-Dashboard/src/pages/general/SubmitProjectPage/SubmitProjectPage.tsx
@@ -1,27 +1,24 @@
 import "./SubmitProjectPage.css"
 import SubmitProjectForm from "../../../components/forms/SubmitProjectForm/SubmitProjectForm.tsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import useLocalStorage from "../../../hooks/useLocalStorage.ts";
 import useAuthenticationCheck from "../../../hooks/useAuthenticationCheck.ts";
 import {Spinner} from "react-bootstrap";
+import ProtectedRoute from "../../../routes/ProtectedRoute.tsx";
 
 function SubmitProjectPage() {
-    const [accessToken] = useLocalStorage<string>('accessToken', '');
-    const [isAuthenticated, setIsAuthenticated] = useAuthenticationCheck(accessToken)
-
-    // Redirect to login page if not authenticated
-    useEffect(() => {
-        if (isAuthenticated === false || accessToken === "" || accessToken === null) {
-            window.location.href = '/login';
-        }
-    }, [isAuthenticated, accessToken]);
-
+    const [loadingAuth, setLoadingAuth] = useState<boolean>(true)
 
     return (
         <>
+            <ProtectedRoute loadingAuth={loadingAuth} setLoadingAuth={setLoadingAuth} />
+
             <div className="submit-project-page">
-                {!isAuthenticated && <Spinner variant="light" animation="grow" />}
-                {isAuthenticated && <SubmitProjectForm/>}
+                {loadingAuth ? (
+                    <span className="loading loading-infinity loading-lg"></span>
+                ) : (
+                    <SubmitProjectForm />
+                )}
             </div>
         </>
     )
