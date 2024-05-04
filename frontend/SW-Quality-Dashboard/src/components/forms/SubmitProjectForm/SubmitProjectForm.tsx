ce2742c9aa40297edc33ee6ee1bea4c227b5542b
Archontis Kostis <arxontisk02@gmail.com>
2024-05-04 19:09:02 +0300

    make Submit Project form protected

--- frontend/SW-Quality-Dashboard/src/components/forms/SubmitProjectForm/SubmitProjectForm.tsx
+++ frontend/SW-Quality-Dashboard/src/components/forms/SubmitProjectForm/SubmitProjectForm.tsx
@@ -17,6 +17,9 @@ function SubmitProjectForm() {
     const [simpleAlertVariant, setSimpleAlertVariant] = useState("info");
     const [simpleAlertIcon, setSimpleAlertIcon] = useState("bi bi-info-circle-fill");
 
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
     const [githubUrl, setGithubUrl] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
@@ -79,6 +82,8 @@ function SubmitProjectForm() {
                 setSimpleAlertHeader(error.response.data.message);
                 setSimpleAlertMessage(error.response.data.exceptionMessage);
 
+                setError(true);
+
                 // Hide the alerts after 10 seconds
                 setTimeout(() => {
                     setShowSimpleAlert(false);
@@ -89,45 +94,145 @@ function SubmitProjectForm() {
 
     return (
         <>
-            <div className="submit-project-form">
-                <h1>
-                    <i className="bi bi-folder-plus"> </i>
-                    Submit a Project
-                </h1>
+                <div className="card shrink-0 w-full max-w-sm bg-base-100 w-100">
+                    <h1
+                        className="text-5xl font-bold m-2"
+                        style={{textAlign: 'center'}}
+                    >
+                        <i className="bi bi-door-open-fill"> </i> Login
+                    </h1>
 
-                {showSimpleAlert &&
-                    <Alert variant={simpleAlertVariant}>
-                        <Alert.Heading>
-                            <i className={simpleAlertIcon}> </i>
-                            {simpleAlertHeader}
-                        </Alert.Heading>
-                        <strong> {simpleAlertMessage} </strong>
-                    </Alert>
-                }
+                    <form className="card-body w-100">
+                        {error &&
+                            <div role="alert" className="alert alert-error">
+                                <i className="bi bi-exclamation-triangle-fill"> </i>
+                                <span> {errorMessage} </span>
+                            </div>
+                        }
 
-                {isLoading && (
-                    <LoadingBar />
-                )}
+                        {/*{loginSuccess &&*/}
+                        {/*    <div role="alert" className="alert alert-success">*/}
+                        {/*        <i className="bi bi-check-circle-fill"> </i>*/}
+                        {/*        <span>*/}
+                        {/*        Login successful! Redirecting...*/}
+                        {/*    </span>*/}
+                        {/*    </div>*/}
+                        {/*}*/}
 
-                <Form className="sign-up-form-content">
-                    <Form.Group controlId="formBasicGithubUrl">
-                        <FloatingFormInput
-                            type="url"
-                            id="githubUrlInput"
-                            placeholder="https://github.com/username/reponame"
-                            isRequired={true}
-                            onChange={(e) => setGithubUrl(e.target.value)}
-                            icon="bi bi-github"
-                            labelText="Github Repo URL"
-                        />
-                    </Form.Group>
 
-                    <Button className="sign-up-form-submit-btn" type="submit" onClick={handleSubmitProject}>
-                        <i className="bi bi-arrow-right-circle-fill"> </i>
-                        Submit Project
-                    </Button>
-                </Form>
-            </div>
+                        <div className="form-control">
+                            <label className="input input-bordered flex items-center gap-2">
+                                <i className="bi bi-envelope-fill"> </i>
+
+                                <input
+                                    type="text"
+                                    className="grow"
+                                    placeholder="Email"
+                                />
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <div className="flex justify-content-evenly">
+                                <label className="input input-bordered flex justify-content-between gap-2"
+                                       style={{width: '100%', margin: 0, padding: 0}}
+                                >
+                                    <i
+                                        className={"bi bi-shield-lock-fill"}
+                                        style={{
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            paddingLeft: '0.5rem',
+                                        }}
+                                    > </i>
+                                    <input
+                                        className="grow"
+                                        placeholder="password"
+                                    />
+
+                                    <div
+                                        className={"btn btn-ghost"}
+
+                                        style={{
+                                            padding: '0.5rem',
+                                        }}
+                                    >
+                                    </div>
+                                </label>
+
+
+                            </div>
+
+                            <label className="label">
+                                <a href="/forgot-password" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label cursor-pointer">
+                                <span className="label-text">
+                                    I am not a <i className={"bi bi-robot"}> </i> (robot).
+                                </span>
+                                <input
+                                    type="checkbox"
+                                    className="checkbox checkbox-primary"
+                                />
+                            </label>
+                        </div>
+
+                        <div className="form-control mt-6">
+                            <button
+                                className="btn btn-primary"
+                            >
+                                {!loading && <i className="bi bi-door-open-fill"> </i>}
+                                {loading && <span className="loading loading-dots loading-xs"></span>}
+                                Login
+                            </button>
+                        </div>
+
+                        <div className="form-control mt-6">
+                            Don't have an account? <a href="/register" className="link">Sign up here.</a>
+                        </div>
+                    </form>
+                </div>
+
+                {/*    <h1>*/}
+                {/*    <i className="bi bi-folder-plus"> </i>*/}
+                {/*    Submit a Project*/}
+                {/*</h1>*/}
+
+                {/*{showSimpleAlert &&*/}
+                {/*    <Alert variant={simpleAlertVariant}>*/}
+                {/*        <Alert.Heading>*/}
+                {/*            <i className={simpleAlertIcon}> </i>*/}
+                {/*            {simpleAlertHeader}*/}
+                {/*        </Alert.Heading>*/}
+                {/*        <strong> {simpleAlertMessage} </strong>*/}
+                {/*    </Alert>*/}
+                {/*}*/}
+
+                {/*{isLoading && (*/}
+                {/*    <LoadingBar />*/}
+                {/*)}*/}
+
+                {/*<Form className="sign-up-form-content">*/}
+                {/*    <Form.Group controlId="formBasicGithubUrl">*/}
+                {/*        <FloatingFormInput*/}
+                {/*            type="url"*/}
+                {/*            id="githubUrlInput"*/}
+                {/*            placeholder="https://github.com/username/reponame"*/}
+                {/*            isRequired={true}*/}
+                {/*            onChange={(e) => setGithubUrl(e.target.value)}*/}
+                {/*            icon="bi bi-github"*/}
+                {/*            labelText="Github Repo URL"*/}
+                {/*        />*/}
+                {/*    </Form.Group>*/}
+
+                {/*    <Button className="sign-up-form-submit-btn" type="submit" onClick={handleSubmitProject}>*/}
+                {/*        <i className="bi bi-arrow-right-circle-fill"> </i>*/}
+                {/*        Submit Project*/}
+                {/*    </Button>*/}
+                {/*</Form>*/}
         </>
     );
 }
