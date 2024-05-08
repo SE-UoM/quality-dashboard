6b2ec14dd3017f997335c148e2434592ade09403
Archontis Kostis <arxontisk02@gmail.com>
2024-05-08 16:13:33 +0300

    Update scrollabl;e rank card

--- frontend/SW-Quality-Dashboard/src/components/dashboard/cards/general/ScrollableRankCard/ScrollableRankCard.tsx
+++ frontend/SW-Quality-Dashboard/src/components/dashboard/cards/general/ScrollableRankCard/ScrollableRankCard.tsx
@@ -2,14 +2,45 @@ import React from 'react';
 import './ScrollableRankCard.css';
 import SimpleDashboardCard from "../../SimpleDashboardCard.tsx";
 
-function ScrollableRankCard({ title, icon, children, cardId, gridArea }) {
+function ScrollableRankCard({ title, icon, children, cardId, gridArea, setAutoScroll, autoscroll }) {
+
     return (
         <SimpleDashboardCard id={cardId}
-            style={{ gridArea: gridArea, height: "100%" }}
+            style={{
+                gridArea: gridArea,
+                height: "100%",
+                width: "100%",
+                padding: "0",
+            }}
         >
-            <h3>
-                <i className={icon}> </i>
-                {title}
+            <h3
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    fontWeight: "bold",
+                    fontSize: "3vh",
+                    width: "100%",
+                    textAlign: "center",
+                    padding: "1.5vh",
+                    margin: "0",
+                    textWrap: "nowrap",
+                }}
+            >
+                <div>
+                    <i className={icon}>&nbsp;</i>
+                    {title}
+                </div>
+
+                <button
+                    onClick={() => {
+                        setAutoScroll(!autoscroll)
+                    }}
+                    className="tooltip tooltip-left btn"
+                    data-tip={"Auto Scroll (" + (autoscroll ? "On" : "Off") + ")"}
+                >
+                    <i className={autoscroll ? "bi bi-pause-circle-fill" : "bi bi-play-circle-fill"}> </i>
+                </button>
             </h3>
 
             <div className="best-projects-list">
