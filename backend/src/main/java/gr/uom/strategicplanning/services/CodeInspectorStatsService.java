fce55e5b28f26a5942fe829ea90ff299896951f3
Archontis Kostis <arxontisk02@gmail.com>
2024-09-19 18:37:37 +0300

    Add a CodeInspectorStats Service to handle the CodeInspectorStats Operations

---
+++ backend/src/main/java/gr/uom/strategicplanning/services/CodeInspectorStatsService.java
@@ -0,0 +1,88 @@
+package gr.uom.strategicplanning.services;
+
+import gr.uom.strategicplanning.models.domain.Organization;
+import gr.uom.strategicplanning.models.domain.Project;
+import gr.uom.strategicplanning.models.external.CodeInspectorProjectStats;
+import gr.uom.strategicplanning.models.stats.CodeInspectorStats;
+import gr.uom.strategicplanning.repositories.CodeInspectorProjectStatsRepository;
+import gr.uom.strategicplanning.repositories.CodeInspectorStatsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class CodeInspectorStatsService {
+    private CodeInspectorProjectStatsRepository codeInspectorProjectStatsRepository;
+    private CodeInspectorStatsRepository codeInspectorStatsRepository;
+
+    @Autowired
+    public CodeInspectorStatsService(CodeInspectorProjectStatsRepository codeInspectorProjectStatsRepository, CodeInspectorStatsRepository codeInspectorStatsRepository) {
+        this.codeInspectorProjectStatsRepository = codeInspectorProjectStatsRepository;
+        this.codeInspectorStatsRepository = codeInspectorStatsRepository;
+    }
+
+    public void updateHotspotAnalysisProjectStats(Project project, Map<String, Object> response) {
+        Map<String, Object> analysis = (Map<String, Object>) response.get("analysis");
+        log.info("Updating Hotspot Analysis Project Stats");
+
+        Optional<CodeInspectorProjectStats> codeInspectorProjectStats = codeInspectorProjectStatsRepository.findByProjectId(project.getId());
+
+        if (codeInspectorProjectStats.isEmpty()) throw new RuntimeException("Project not found in CodeInspectorProjectStats");
+
+        Collection<Map> hotspots = (Collection<Map>) analysis.get("prioritized_files");
+        int totalHotspots = hotspots.size();
+
+        Collection<Map> outliers = (Collection<Map>) analysis.get("outliers");
+        int totalOutliers = outliers.size();
+
+        CodeInspectorProjectStats updatedProjectStats = codeInspectorProjectStats.get();
+        updatedProjectStats.setProjectUrl(project.getRepoUrl());
+        updatedProjectStats.setProjectName(project.getName());
+        updatedProjectStats.setFromDate((String) analysis.get("from_date"));
+        updatedProjectStats.setToDate((String) analysis.get("to_date"));
+        updatedProjectStats.setAverageChurn((Double) analysis.get("avg_churn"));
+        updatedProjectStats.setTotalOutliers(totalOutliers);
+        updatedProjectStats.setTotalHotspots(totalHotspots);
+
+        codeInspectorProjectStatsRepository.save(updatedProjectStats);
+    }
+
+    public void updateOrganizationStats(Organization org) {
+        log.info("Updating Organization Stats");
+
+        Long organizationAnalysisId = org.getOrganizationAnalysis().getId();
+        Optional<CodeInspectorStats> codeInspectorStats = codeInspectorStatsRepository.findByOrganizationAnalysisId(organizationAnalysisId);
+
+        if (codeInspectorStats.isEmpty()) throw new RuntimeException("Organization not found in CodeInspectorStats");
+
+        CodeInspectorStats updatedStats = codeInspectorStats.get();
+
+        List<Project> organizationProjects = org.getProjects();
+
+        int totalHotspots = 0;
+        int totalOutliers = 0;
+
+        for(Project project : organizationProjects) {
+            Long projectID = project.getId();
+            Optional<CodeInspectorProjectStats> projectStats = codeInspectorProjectStatsRepository.findByProjectId(projectID);
+
+            if (projectStats.isEmpty()) throw new RuntimeException("Project not found in CodeInspectorProjectStats");
+
+            totalHotspots += projectStats.get().getTotalHotspots();
+            totalOutliers += projectStats.get().getTotalOutliers();
+
+            updatedStats.addGitUrl(project.getRepoUrl());
+        }
+
+        updatedStats.setTotalHotspots(totalHotspots);
+        updatedStats.setTotalOutliers(totalOutliers);
+
+        codeInspectorStatsRepository.save(updatedStats);
+    }
+}
