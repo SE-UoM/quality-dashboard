fd5d44ae529dc9a45049553cafeb1c269f14459b
Archontis Kostis <arxontisk02@gmail.com>
2024-09-29 22:24:40 +0300

    update coverage cards

--- frontend/SW-Quality-Dashboard/src/components/dashboard/cards/CoverageCard.tsx
+++ frontend/SW-Quality-Dashboard/src/components/dashboard/cards/CoverageCard.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import SimpleDashboardCard from "./SimpleDashboardCard.tsx";
 
-function CoverageCard({gridArea, iconA, iconB, iconC, headA, headB, headC, valueA, valueB, valueC, loading}) {
+function CoverageCard({ gridArea, stats, loading }) {
     return (
         <>
             {loading ? (
                 <SimpleDashboardCard
                     className="skeleton"
                     id={"coverageCard"}
-                    style={{height: "100%", gridArea: gridArea}}
+                    style={{ height: "100%", gridArea: gridArea }}
                 />
-                ) : (
+            ) : (
                 <div
                     className="stats shadow bg-base-200"
                     id=""
@@ -20,39 +20,22 @@ function CoverageCard({gridArea, iconA, iconB, iconC, headA, headB, headC, value
                         padding: "0",
                     }}
                 >
-                    <div className="stat">
-                        <div className="stat-figure text-info">
-                            {iconA}
+                    {stats.map((stat, index) => (
+                        <div className="stat" key={index}>
+                            <div className={`stat-figure text-${stat.color}`}>
+                                {stat.icon}
+                            </div>
+                            <div className="stat-title">{stat.title}</div>
+                            <div className="stat-value">{stat.value}</div>
+                            <div className="stat-desc">
+                                {stat.desc}
+                            </div>
                         </div>
-                        <div className="stat-title">
-                            {headA}
-                        </div>
-                        <div className="stat-value">{valueA}</div>
-                    </div>
-
-                    <div className="stat">
-                        <div className="stat-figure text-success">
-                            {iconB}
-                        </div>
-                        <div className="stat-title">
-                            {headB}
-                        </div>
-                        <div className="stat-value">{valueB}</div>
-                    </div>
-
-                    <div className="stat">
-                        <div className="stat-figure text-error">
-                            {iconC}
-                        </div>
-                        <div className="stat-title">
-                            {headC}
-                        </div>
-                        <div className="stat-value">{valueC}</div>
-                    </div>
+                    ))}
                 </div>
             )}
         </>
-    )
+    );
 }
 
 export default CoverageCard;
