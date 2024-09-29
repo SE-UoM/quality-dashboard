4161fe075a962db1bc0093caa05ee01df6bfb31b
Archontis Kostis <arxontisk02@gmail.com>
2024-09-29 22:23:24 +0300

    add default values

--- frontend/SW-Quality-Dashboard/src/components/dashboard/slides/DashboardSlideFive/DashboardSlideFive.tsx
+++ frontend/SW-Quality-Dashboard/src/components/dashboard/slides/DashboardSlideFive/DashboardSlideFive.tsx
@@ -1,83 +1,118 @@
 import './DashboardSlideFive.css';
 import '../DashboardSlideStyle.css';
-
 import FooterCard from "../../cards/general/FooterCard/FooterCard.tsx";
 import useLocalStorage from "../../../../hooks/useLocalStorage.ts";
-
-import {formatText} from "../../../../utils/textUtils.ts";
+import { formatText } from "../../../../utils/textUtils.ts";
 import SimpleDashboardCard from "../../cards/SimpleDashboardCard.tsx";
 import DashboardDonutChart from "../../../charts/DashboardDonutChart.tsx";
-import React, {useEffect} from "react";
-import {BarList, CategoryBar, LineChart} from "@tremor/react";
-import {VscIssues} from "react-icons/vsc";
+import React, { useEffect } from "react";
+import { BarList, CategoryBar, LineChart } from "@tremor/react";
+import { VscIssues } from "react-icons/vsc";
 import IconCard from "../../cards/screen1/IconCard/IconCard.tsx";
 import CommitsQualityPie from "../../cards/CommitsQualityPie/CommitsQualityPie.tsx";
 import useAxiosGet from "../../../../hooks/useAxios.ts";
 import apiUrls from "../../../../assets/data/api_urls.json";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import CommitsActivity from "../../cards/CommitsActivity.tsx";
-import {TbChartRadar} from "react-icons/tb";
-import {TiRadar} from "react-icons/ti";
+import { TbChartRadar } from "react-icons/tb";
+import { TiRadar } from "react-icons/ti";
 import CommitHeatmap from "../../../charts/CommitHeatmap/CommitHeatmap.tsx";
 import IssuesBar from "../../cards/IssuesBar.tsx";
 import HotspotsRadar from "../../cards/HotspotsRadar.tsx";
 import CoverageCard from "../../cards/CoverageCard.tsx";
 
-const baseApiUrl = import.meta.env.VITE_API_BASE_URL
+const baseApiUrl = import.meta.env.VITE_API_BASE_URL;
 
-function DashboardSlideFive({orgID}) {
-    const {data: hotspotsData, loading: hotspotsLoading, error: hotspotsError, errorMessage: hotspotsErrorMessage} =
+function DashboardSlideFive({ orgID }) {
+    const { data: hotspotsData, loading: hotspotsLoading } =
         useAxiosGet(baseApiUrl + apiUrls.routes.dashboard.hotspotsDistribution.replace(":organizationId", orgID), "");
 
     const [hotspotsQualityData, setHotspotsQualityData] = React.useState(null);
     const [totalHotspots, setTotalHotspots] = React.useState(0);
 
-    const {data: coverageData, loading: coverageLoading, error: coverageError, errorMessage: coverageErrorMessage} =
+    const { data: coverageData, loading: coverageLoading } =
         useAxiosGet(baseApiUrl + apiUrls.routes.dashboard.coverage.replace(":organizationId", orgID), "");
 
-    const {data: refactoringsData, loading: refactoringsLoading, error: refactoringsError, errorMessage: refactoringsErrorMessage} =
+    const { data: refactoringsData, loading: refactoringsLoading } =
         useAxiosGet(baseApiUrl + apiUrls.routes.dashboard.refactorings.replace(":organizationId", orgID), "");
 
-    const {data: dependenciesData, loading: dependenciesLoading, error: dependenciesError, errorMessage: dependenciesErrorMessage} =
+    const { data: dependenciesData, loading: dependenciesLoading } =
         useAxiosGet(baseApiUrl + apiUrls.routes.dashboard.dependencies.replace(":organizationId", orgID), "");
 
-    // Effect to transform the hotspots data once hotspotsData is fetched
     React.useEffect(() => {
         if (hotspotsData) {
-            let normal = hotspotsData.normalPriorityHotspots;
-            let low = hotspotsData.lowPriorityHotspots;
-            let high = hotspotsData.highPriorityHotspots;
-            let medium = hotspotsData.mediumPriorityHotspots;
-            let unknown = hotspotsData.unknownPriorityHotspots;
+            let normal = hotspotsData.normalPriorityHotspots || 0;
+            let low = hotspotsData.lowPriorityHotspots || 0;
+            let high = hotspotsData.highPriorityHotspots || 0;
+            let medium = hotspotsData.mediumPriorityHotspots || 0;
+            let unknown = hotspotsData.unknownPriorityHotspots || 0;
 
             let total = normal + low + high + medium + unknown;
 
-            // Transforming the data into the desired structure
-            // The format is [HIGH, MEDIUM, NORMAL, UNKNOWN, LOW]
-            const formattedData = [
-                high,
-                medium,
-                normal,
-                unknown,
-                low
-            ];
+            const formattedData = [high, medium, normal, unknown, low];
+            const series = [{ name: 'Hotspots Priority', data: formattedData }];
 
-            const series = [{
-                name: 'Hotspots Priority',
-                data: formattedData,
-            }];
-
-            // Set the formatted data to the state
             setHotspotsQualityData(series);
             setTotalHotspots(total);
         }
-
     }, [hotspotsData]);
+
+    // Create the stats array dynamically based on the data fetched
+    const coverageStats = [
+        {
+            icon: <i className={"bi bi-code-slash"} style={{ fontSize: '6vh' }}></i>,
+            title: "Statements",
+            value: coverageData?.totalStmts ? formatText(coverageData.totalStmts, 'k') : 0, // Safe access with optional chaining
+            color: "secondary",
+            desc: "*for Python files"
+        },
+        {
+            icon: <i className={"bi bi-shield-check"} style={{ fontSize: '6vh' }}></i>,
+            title: "Covered",
+            value: coverageData?.totalCoverage ? formatText(coverageData.totalCoverage, 'k') : 0, // Safe access with optional chaining
+            color: "primary",
+            desc: "*for Python files"
+        },
+        {
+            icon: <i className={"bi bi-shield-x"} style={{ fontSize: '6vh' }}></i>,
+            title: "Missed",
+            value: coverageData?.totalMiss ? formatText(coverageData.totalMiss, 'k') : 0, // Safe access with optional chaining
+            color: "success",
+            desc: "*for Python files"
+        },
+        {
+            icon: <i className={"bi bi-filetype-py"} style={{ fontSize: '6vh' }}></i>,
+            title: "Dependencies",
+            value: dependenciesData?.totalDependencies ? formatText(dependenciesData.totalDependencies, 'k') : 0, // Safe access with optional chaining
+            color: "accent",
+            desc: "*for Python files"
+        }
+    ];
+
+    const hotspotsCardData = [
+        {
+            icon: <i className={"bi bi-exclamation-octagon text-warning"} style={{ fontSize: '6vh' }}></i>,
+            title: "Hotspot Files",
+            value: formatText(totalHotspots, "k"),
+            color: "warning",
+            desc: "by CodeInspector"
+        }
+    ];
+
+    const refactoringsCardData = [
+        {
+            icon: <i className={"bi bi-wrench-adjustable-circle text-success"} style={{ fontSize: '6vh' }}></i>,
+            title: "Refactorings",
+            value: formatText(refactoringsData ? refactoringsData.totalRefactorings : 0, "k"),
+            color: "success",
+            desc: "by RefactoringMiner"
+        }
+    ]
 
     return (
         <>
             <div className="dashboard-slide" id="slide5">
-                <CommitsQualityPie orgID={orgID} gridArea='commitQualityDistribution'/>
+                <CommitsQualityPie orgID={orgID} gridArea='commitQualityDistribution' />
                 <HotspotsRadar
                     gridArea='radarChart'
                     data={hotspotsData ? hotspotsQualityData : null}
@@ -86,49 +121,30 @@ function DashboardSlideFive({orgID}) {
 
                 <CoverageCard
                     gridArea="coverageCard"
-                    headA={"Statements"}
-                    valueA={coverageData ? formatText(coverageData.totalStmts, 'k') : 0}
-                    iconA={<i className={"bi bi-code-slash"} style={{fontSize: '6vh'}}> </i>}
-
-                    headB={"Covered"}
-                    valueB={coverageData ? formatText(coverageData.totalCoverage, 'k') : 0}
-                    iconB={<i className={"bi bi-shield-check"} style={{fontSize: '6vh'}}> </i>}
-
-                    headC={"Missed"}
-                    valueC={coverageData ? formatText(coverageData.totalMiss, 'k') : 0}
-                    iconC={<i className={"bi bi-shield-x"} style={{fontSize: '6vh'}}> </i>}
-
+                    stats={coverageStats}
                     loading={coverageLoading}
                 />
 
                 <CoverageCard
-                    gridArea="statCard"
-
-                    headA={"Hotspot Files"}
-                    valueA={formatText(totalHotspots, "k")}
-                    iconA={<i className={"bi bi-exclamation-octagon text-warning"} style={{fontSize: '6vh'}}> </i>}
-
-                    headB={"Refactorings"}
-                    valueB={formatText(refactoringsData ? refactoringsData.totalRefactorings : 0, "k")}
-                    iconB={<i className={"bi bi-wrench-adjustable-circle text-success"} style={{fontSize: '6vh'}}> </i>}
-
-                    headC={"Python Deps"}
-                    valueC={formatText(dependenciesData ? dependenciesData.totalDependencies : 0, "k")}
-                    iconC={<i className={"bi bi-filetype-py text-accent"} style={{fontSize: '6vh'}}> </i>}
-
+                    gridArea="hotspotsTotal"
+                    stats={hotspotsCardData}
                     loading={hotspotsLoading || refactoringsLoading || dependenciesLoading}
                 />
-                <CommitHeatmap gridArea='commitHeatmap' orgID={orgID}/>
-                <CommitsActivity gridArea='commitsTimeline' orgID={orgID}/>
-                <IssuesBar gridArea='issues' orgID={orgID}/>
 
-                {/*Make it cover the whole row*/}
-                <FooterCard
-                    gridAreaName="footerCard"
+                <CoverageCard
+                    gridArea="refactoringsTotal"
+                    stats={refactoringsCardData}
+                    loading={refactoringsLoading}
                 />
+
+                <CommitHeatmap gridArea='commitHeatmap' orgID={orgID} />
+                <CommitsActivity gridArea='commitsTimeline' orgID={orgID} />
+                <IssuesBar gridArea='issues' orgID={orgID} />
+
+                <FooterCard gridAreaName="footerCard" />
             </div>
         </>
-    )
+    );
 }
 
 export default DashboardSlideFive;
